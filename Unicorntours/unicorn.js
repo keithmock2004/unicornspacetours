d2d15c166c873dd034bc198c0c53f9131b3f4eb3
Keith <78268988+keithmock2004@users.noreply.github.com>
2023-07-17 12:06:05 -0500

    committing changes

---
+++ Unicorntours/unicorn.js
@@ -0,0 +1,55 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+
+const paths = {
+  styles: {
+    src: ["./src/*.css"],
+    dest: "./dist/"
+  },
+  html: {
+    src: ["./src/*.html"],
+    dest: "./dist/"
+  }
+
+  img:{
+
+    src:["./src/img/*"],
+    dest: "./dist/",
+  }
+};
+
+/* STYLES */
+function style() {
+  return gulp
+    .src(paths.styles.src)
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browserSync.stream());
+}
+
+/* HTML */
+function html() {
+  return gulp
+    .src(paths.html.src)
+    .pipe(gulp.dest(paths.html.dest))
+    .pipe(browserSync.stream());
+}
+function html() {
+  return gulp
+    .src(paths.img.src)
+    .pipe(gulp.dest(paths.img.dest))
+    .pipe(browserSync.stream());
+
+/* FUNCTIONS */
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: "./src/"
+    }
+  });
+  gulp.watch(paths.styles.src, style);
+  gulp.watch(paths.html.src, html);
+  gulp.watch(paths.img.src, html);
+}
+
+/* GULP TASKS */
+exports.default = watch;
